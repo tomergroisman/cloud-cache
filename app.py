c2dc8713ec83d283daf25dbbc87846da8b4a923b
Tomer Groisman <tomergroisman@gmail.com>
2021-06-18 11:24:44 +0000

    Add initial cache routes

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, Response
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return flask.Response(status=200)
+@app.route('/put')
+def put_to_cache():
+    return "in put"
+
+@app.route('/get')
+def get_from_cache():
+    return "in get"
 
 @app.route('/health-check')
 def health_check():
-    return 'Hello World!'
+    return Response(status=200)
