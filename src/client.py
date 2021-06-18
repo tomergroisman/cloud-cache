7b8965fcaae3da7ffa88b7f4994a1141f16a4e8e
Tomer Groisman <tomergroisman@gmail.com>
2021-06-18 13:46:27 +0000

    Add sort feature to healthy nodes list

--- src/client.py
+++ src/client.py
@@ -1,6 +1,6 @@
 import boto3
 
-from utils import get_instance_id, filter_healthy
+from utils import get_instance_id, filter_healthy, sort_by_id
 
 target_group_arn = "arn:aws:elasticloadbalancing:eu-central-1:217568182542:targetgroup/Instances/0994232bb9fdf1f8"
 
@@ -11,5 +11,6 @@ class ELB_Client:
 
   def get_healthy_nodes(self):
     all_targets = self.client.describe_target_health(TargetGroupArn=target_group_arn)["TargetHealthDescriptions"]
-    healthy_targets= list(filter(filter_healthy, all_targets))
+    healthy_targets = list(filter(filter_healthy, all_targets))
+    sorted_healthy_targets = sort_by_id(healthy_targets)
     return healthy_targets
--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
 import urllib.request
+import functools
 
 AWS_GET_INSTANCE_ID_ENDPOINT = "http://169.254.169.254/latest/meta-data/instance-id"
 
@@ -6,11 +7,17 @@ def get_instance_id():
   instance_id = urllib.request.urlopen(AWS_GET_INSTANCE_ID_ENDPOINT).read().decode()
   return instance_id
 
+def get_target_id(target):
+  return target["Target"]["Id"]
+
+def sort_by_id(targets):
+  return sorted(targets, key=get_target_id)
+
 def filter_healthy(target):
   return target["TargetHealth"]["State"] == "healthy"
 
 def filter_other_healthy(instance_id, targets):
-  other_targets = [target for target in targets if not target["Target"]["Id"] == instance_id]
+  other_targets = [target for target in targets if not get_target_id(target) == instance_id]
   return list(filter(filter_healthy, other_targets))
   
   
